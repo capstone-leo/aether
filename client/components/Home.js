f4e7553f625b51e007041f91b4ffe91d9e6be9b5
Sebastian Leighton <68569610+SL8onC@users.noreply.github.com>
2021-02-05 01:02:33 -0500

    styling (buttons and some accessibility functions)

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Particles from 'react-particles-js';
 
@@ -8,14 +8,13 @@ import 'firebase/auth';
 
 // Initialize Firebase
 firebase.initializeApp({
-  apiKey: 'AIzaSyCL4dmyWs2djZQWA7SkJQM06ket2Z0VzJw',
-  authDomain: 'music-visual-jam-sesh.firebaseapp.com',
-  databaseURL: 'https://music-visual-jam-sesh-default-rtdb.firebaseio.com',
-  projectId: 'music-visual-jam-sesh',
-  storageBucket: 'music-visual-jam-sesh.appspot.com',
-  messagingSenderId: '56740385434',
-  appId: '1:56740385434:web:1f60538266d67451dc4523',
-  measurementId: 'G-QGC5XHSDST',
+	apiKey: 'AIzaSyAWVIxjZTaEY90wBDSxj1v7ECUUKyG7Vb0',
+	authDomain: 'sound-collabo.firebaseapp.com',
+	projectId: 'sound-collabo',
+	storageBucket: 'sound-collabo.appspot.com',
+	messagingSenderId: '564645648142',
+	appId: '1:564645648142:web:f0b8c196f95fd2b70c295f',
+	measurementId: 'G-V44HEGZ64L'
 });
 // Initialize auth and firestore
 export const auth = firebase.auth();
@@ -23,87 +22,152 @@ export const db = firebase.firestore();
 
 // Main Component
 const Home = () => {
-  const [showInstructions, setShowInstructions] = useState(false);
+	useEffect(() => {
+		window.addEventListener('keydown', handleKeydown);
+		// console.log(params);
+	});
 
-  const showDirections = (event) => {
-    event.preventDefault();
+	const [enableOutline, setenableOutline] = useState(false);
 
-    setShowInstructions(!showInstructions);
-  };
-  return (
-    <>
-      <div style={{ textAlign: 'center', marginTop: '15%', fontSize: '60px' }}>
-        <header>a e t h e r</header>
-        <p style={{ fontSize: '15px' }}>(working title)</p>
-        <SignIn />
-        <SignOut />
-        <br />
-        <Link to="/sesh">
-          <button
-            type="button"
-            style={{ textAlign: 'center', marginTop: '1%' }}
-          >
-            Start Jamming {'>'}
-          </button>
-        </Link>
-        <br />
-        <button
-          type="button"
-          onClick={showDirections}
-          style={{ textAlign: 'center', marginTop: '1%', fontSize: '15px' }}
-        >
-          view instructions
-        </button>
+	const [showInstructions, setShowInstructions] = useState(false);
 
-        {showInstructions ? (
-          <ol style={{ textAlign: 'center', fontSize: '15px' }}>
-            <li>“One band. One Sound.”</li>
-            <li>"If you ain't first, you're last" </li>
-            <li>"You shall not pass!"</li>
-            <li>"I am McLovin"</li>
-            <li>
-              "Life is like a box of chocolates, you never know what you're
-              gonna get"
-            </li>
-          </ol>
-        ) : null}
-        <br />
+	//Accessibility Handler
+	const handleKeydown = (e) => {
+		const isTabEvent = e.keyCode === 9;
+		if (isTabEvent) {
+			setenableOutline(true);
+		}
+	};
 
-        <Particles
-          params={{
-            particles: {
-              line_linked: {
-                shadow: {
-                  enable: true,
-                  color: '#3CA9D1',
-                  blur: 5,
-                },
-              },
-            },
-          }}
-          style={{
-            width: '100%',
-          }}
-        />
-      </div>
-    </>
-  );
+	//Instructions Toggle
+	const showDirections = (event) => {
+		event.preventDefault();
+
+		setShowInstructions(!showInstructions);
+	};
+
+	// JSX
+	return (
+		<>
+			<div style={{ textAlign: 'center', marginTop: '15%', fontSize: '60px' }}>
+				<Particles
+					id='particles-js'
+					params={{
+						particles: {
+							line_linked: {
+								shadow: {
+									enable: true,
+									color: `${randomColor}`,
+									blur: 2
+								}
+							},
+							interactivity: {
+								detect_on: 'canvas',
+								events: {
+									onhover: {
+										enable: true,
+										mode: 'grab'
+									},
+									resize: true
+								},
+								modes: {
+									grab: {
+										distance: 400,
+										line_linked: {
+											opacity: 1
+										}
+									}
+								}
+							},
+							move: {
+								enable: true,
+								speed: 0.6,
+								direction: 'bottom-left',
+								random: false,
+								straight: false,
+								out_mode: 'bounce',
+								bounce: false,
+								attract: {
+									enable: false,
+									rotateX: 600,
+									rotateY: 1200
+								}
+							},
+							retina_detect: true
+						}
+					}}
+				/>
+				<header style={{ textShadow: '0px 2px 6 rgba(218, 217, 217, 0.346)' }}>
+					a e t h e r
+				</header>
+				<br />
+				<Link to='/sesh'>
+					<button
+						className={enableOutline ? 'home-btn' : 'no-outline-on-focus home-btn'}
+						type='button'
+						style={{ textAlign: 'center', marginTop: '1%' }}
+					>
+						Start Jamming {'>'}
+					</button>
+				</Link>
+				<br />
+				<SignIn enableOutline={enableOutline} />
+				<SignOut enableOutline={enableOutline} />
+				<br />
+				<button
+					type='button'
+					className={enableOutline ? 'home-btn' : 'no-outline-on-focus home-btn'}
+					onClick={showDirections}
+					style={{ textAlign: 'center', marginTop: '1%', fontSize: '12px' }}
+				>
+					view instructions
+				</button>
+				{showInstructions ? (
+					<ol style={{ textAlign: 'center', fontSize: '15px' }}>
+						<li>“One band. One Sound.”</li>
+						<li>"If you ain't first, you're last" </li>
+						<li>"You shall not pass!"</li>
+						<li>"I am McLovin"</li>
+						<li>
+							"Life is like a box of chocolates, you never know what you're gonna get"
+						</li>
+					</ol>
+				) : null}
+				<br />
+			</div>
+		</>
+	);
 };
 
 // Helper Components
-function SignIn() {
-  const signInWithGoogle = () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    //instatiate new auth token
-    auth.signInWithPopup(provider);
-    //prompts separate window to google login
-  };
+function SignIn(props) {
+	const signInWithGoogle = () => {
+		const provider = new firebase.auth.GoogleAuthProvider();
+		//instatiate new auth token
+		auth.signInWithPopup(provider);
+		//prompts separate window to google login
+	};
 
-  return <button onClick={signInWithGoogle}>Sign in with Google</button>; // button to prompt Google login
+	return (
+		<button
+			className={props.enableOutline ? 'home-btn' : 'no-outline-on-focus home-btn'}
+			onClick={signInWithGoogle}
+		>
+			Sign in with Google
+		</button>
+	); // button to prompt Google login
 }
-function SignOut() {
-  return auth.currentUser ? (
-    <button onClick={() => auth.signOut()}>Sign Out</button>
-  ) : null;
+function SignOut(props) {
+	return auth.currentUser ? (
+		<button
+			className={props.enableOutline ? 'home-btn' : 'no-outline-on-focus home-btn'}
+			onClick={() => auth.signOut()}
+		>
+			Sign Out
+		</button>
+	) : null;
 }
+
+// Helper Function
+const randomColor = Math.floor(Math.random() * 16777215).toString(16);
 export default Home;
