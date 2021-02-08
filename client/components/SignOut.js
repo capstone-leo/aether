a7057a26e6f1d611d7c125f49b1c67daf28a7a30
Sebastian Leighton <68569610+SL8onC@users.noreply.github.com>
2021-02-08 03:53:01 -0500

    refactor: Sign In components are now separate from Home

---
+++ client/components/SignOut.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { signOutUser } from '../reducer/user';
+import { connect } from 'react-redux';
+import { firebaseApp, auth, db } from '../Firebase';
+
+const SignOut = (props) => {
+	const signOut = () => {
+		auth.signOut();
+		props.signOutUser(props.user);
+	};
+
+	return props.user ? (
+		<button
+			className={props.enableOutline ? 'home-btn' : 'no-outline-on-focus home-btn'}
+			onClick={() => signOut()}
+		>
+			Sign Out
+		</button>
+	) : null;
+};
+
+// Connect Redux
+const mapStateToProps = (state) => ({ user: state.user });
+const mapDispatchToProps = (dispatch) => ({
+	signOutUser: (user) => dispatch(signOutUser(user))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignOut);
