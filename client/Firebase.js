7f190ce876fa609092c8335b1de06546acd134ed
Sebastian Leighton <68569610+SL8onC@users.noreply.github.com>
2021-02-09 17:23:36 -0500

    refactor: Firebase secrets

--- client/Firebase.js
+++ client/Firebase.js
@@ -1,19 +1,10 @@
 import firebase from 'firebase';
 import 'firebase/auth';
 import 'firebase/firestore';
+import firebaseConfig from '../config/secrets';
 
 // Initialize Firebase
-export const firebaseApp = firebase.initializeApp({
-	apiKey: 'AIzaSyAWVIxjZTaEY90wBDSxj1v7ECUUKyG7Vb0',
-	authDomain: 'sound-collabo.firebaseapp.com',
-	projectId: 'sound-collabo',
-	storageBucket: 'sound-collabo.appspot.com',
-	messagingSenderId: '564645648142',
-	appId: '1:564645648142:web:f0b8c196f95fd2b70c295f',
-	measurementId: 'G-V44HEGZ64L'
-});
-
-// Initialize auth and firestore
+export const firebaseApp = firebase.initializeApp(firebaseConfig.firebaseConfig);
 export const auth = firebase.auth();
 export const db = firebase.firestore();
 export const realtimeDB = firebase.database();
