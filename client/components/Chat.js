51e74c576c3c712edd99512a2d02f18033328412
Paul Neenan <paulmneenan@gmail.com>
2021-02-12 14:21:22 -0500

    feat: chat names

--- client/components/Chat.js
+++ client/components/Chat.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState, useRef } from 'react';
 import { connect } from 'react-redux';
 import socket from '../socket';
-import store from '../store'
-import receiveMessage, { deleteMessage } from '../reducer/messages'
-import { nanoid } from 'nanoid'
+import store from '../store';
+import receiveMessage, { deleteMessage } from '../reducer/messages';
+import { nanoid } from 'nanoid';
 // import nanoid from 'nanoid'
 
 //once we start having usernames/nicknames this needs
@@ -11,64 +11,72 @@ import { nanoid } from 'nanoid'
 
 export const Chat = (props) => {
   const { messages } = props;
-  const [message, setMessage] = useState('')
+  const [message, setMessage] = useState('');
+  const [name, setName] = useState('');
+  const [isNameSet, setIsNameSet] = useState(false);
 
-
-   useEffect(() => { //props.messages.length ?
-    // document.querySelector('li') !== null ?
-    //   // ? setTimeout(function () {
-    //       removeMessage()
-    //       //document.querySelector('li').remove();
-    //       // setTimeOut(function () {
-    //       //   removeMessage()
-    //     // }, 1000)
-    //   : null
-  });
+  //  useEffect(() => { //props.messages.length ?
+  //   // document.querySelector('li') !== null ?
+  //   //   // ? setTimeout(function () {
+  //   //       removeMessage()
+  //   //       //document.querySelector('li').remove();
+  //   //       // setTimeOut(function () {
+  //   //       //   removeMessage()
+  //   //     // }, 1000)
+  //   //   : null
+  // });
 
   const sendMessage = () => {
-      socket.emit('add_message', {message: message, id: nanoid()});
-      setMessage('')
-      console.log('first store', store.getState().messages)
-      document.getElementById('new-message').value=''
-      setTimeout(function () {
-        removeMessage(message, store.getState().messages)
-      }, 4000)
-    }
-  
+    socket.emit('add_message', { message: message, id: nanoid() });
+    setMessage('');
+    console.log('first store', store.getState().messages);
+    document.getElementById('new-message').value = '';
+    setTimeout(function () {
+      removeMessage(message, store.getState().messages);
+    }, 4000);
+  };
 
-  const messageList = 
-  store.getState().messages.map((message, i)=>
-  <li key={i}>{message.message}</li>)
+  const onNameSubmit = () => {
+
+    setIsNameSet(true);
+    document.getElementById('new-message').value = '';
+  };
+
+  const messageList = store
+    .getState()
+    .messages.map((message, i) => <li key={i}>{message.message}</li>);
 
   const removeMessage = (message, id) => {
-    socket.emit('remove_message', id)
-    // setTimeout(function () {
-    //   // document.querySelector('li').remove();
-    //   console.log('latemessages', store.getState().messages)
-    // }, 3000)
-  }
-  
+    socket.emit('remove_message', id);
+  };
 
-  return (
-    <div id="chat-box">
-      <ul id="message-list">
-        {messageList}
-      </ul>
+  return isNameSet ? (
+    <div id='chat-box'>
+      <ul id='message-list'>{messageList}</ul>
       <input
         style={{ background: 'transparent', color: 'whitesmoke' }}
-        // value={message}
-        id="new-message"
+        id='new-message'
         onInput={(e) => {
-          setMessage(e.target.value);
-         
+          setMessage(`${name}: ` + e.target.value);
         }}
         onKeyDown={(e) => (e.key === 'Enter' ? sendMessage() : null)}
-        placeholder="chat"
+        placeholder='chat'
+      ></input>
+    </div>
+  ) : (
+    <div id='chat-box'>
+      <ul id='message-list'>{messageList}</ul>
+      <input
+        style={{ background: 'transparent', color: 'whitesmoke' }}
+        id='new-message'
+        onChange={(e)=>setName(e.target.value)}
+        onKeyDown={(e) => (e.key === 'Enter' ? onNameSubmit() : null)}
+        placeholder='enter name to start chat'
       ></input>
     </div>
   );
 };
 
-const mapStateToProps = (messages) => (messages);
+const mapStateToProps = (messages) => messages;
 
 export default connect(mapStateToProps, null)(Chat);
