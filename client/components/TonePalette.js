f25bcc44373147379f2b44e19dcda5ea2f5a675c
Joe Young <musicjoeyoung@gmail.com>
2021-02-15 14:59:40 -0500

    style: tone-palette fix; made chat placeholder text white

--- client/components/TonePalette.js
+++ client/components/TonePalette.js
@@ -9,17 +9,19 @@ export class TonePalette extends Component {
   render() {
     return (
       <div className="dropdown">
-        <span id="spanHover">
-          <div className="TonePalette">
-            <img src="./star.png" id="addInstrumentIcon"></img>
-            <img src="./drum.png" id="drumIcon"></img>
-            <img src="./piano.png" id="pianoIcon"></img>
-            <img src="./chord2.png" id="chordIcon"></img>
-            <img src="./xylophone.png" id="marimbaIcon"></img>
-            <img src="./harp.png" id="harpIcon"></img>
-            <img src="./feedbackDelay.png" id="feedbackDelayIcon"></img>
-          </div>
-        </span>
+        <div id="divHover">
+          hover for sounds
+          <br />↓
+        </div>
+        <div className="TonePalette">
+          <img src="./star.png" id="addInstrumentIcon"></img>
+          <img src="./drum.png" id="drumIcon"></img>
+          <img src="./piano.png" id="pianoIcon"></img>
+          <img src="./chord2.png" id="chordIcon"></img>
+          <img src="./xylophone.png" id="marimbaIcon"></img>
+          <img src="./harp.png" id="harpIcon"></img>
+          <img src="./feedbackDelay.png" id="feedbackDelayIcon"></img>
+        </div>
       </div>
     )
   }
